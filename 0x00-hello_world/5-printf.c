91024e9826bec49fc35af200428b82cf2a892bc2
Jerry Jehu <jerryjehuprecious@gmail.com>
2022-09-08 16:19:22 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  *  * main-entry point for the program.
  *   *
  *    * Return:0 if no errors, return non zero if errors
- *     */
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
