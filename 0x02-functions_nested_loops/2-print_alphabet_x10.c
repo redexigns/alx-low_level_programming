6d4143330dcf60c74d91f72b0928474b77f066c5
Jerry Jehu <jerryjehuprecious@gmail.com>
2022-09-13 16:21:26 -0700

    Ten

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,13 @@ void print_alphabet_x10(void)
 	int ch;
 	int i = 0;
 
-	while (i <= 0)
+	for (i = 0; i <=9; i++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 		{
-			putchar(ch);
+			_putchar(ch);
 		}
 
-		putchar('\n');
-
-	i++;
+		_putchar('\n');
+	}
 }
