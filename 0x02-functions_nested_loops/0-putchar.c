98c66ac5f981b17a6a91acbbc05970af21a0612b
Jerry Jehu <jerryjehuprecious@gmail.com>
2022-09-13 10:20:14 -0700

    Header File

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints keyword _putchar
+ *
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar("\n");
+
+return 0;
+}
