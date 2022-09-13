b77620a64f9f666bc645513834b17ba5575834c7
Jerry Jehu <jerryjehuprecious@gmail.com>
2022-09-13 15:50:38 -0700

    Ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet
+ * check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i = 0;
+
+	while (i <= 0)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
+
+		putchar('\n');
+
+	i++;
+}
