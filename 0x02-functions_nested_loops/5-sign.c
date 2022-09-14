54cd410a75cd35443bb395efcea72b15f74d760a
Jerry Jehu <jerryjehuprecious@gmail.com>
2022-09-13 17:01:27 -0700

    n

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar("+");
+	_putchar('+');
 	return (1);
 }
 	else if (n < 0)
@@ -19,6 +19,6 @@ int print_sign(int n)
 	else
 	{
 	_putchar('0');
-	return (8);
+	return (0);
 	}
 }
