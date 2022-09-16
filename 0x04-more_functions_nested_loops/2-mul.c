8ba5b2a4c0c39bb6e1e1565d71f050dc77ea4f60
Jerry Jehu <jerryjehuprecious@gmail.com>
2022-09-15 18:16:27 -0700

    Multiples Integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: first number 
+ * @b: second number
+ * Return: reti\urns result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
