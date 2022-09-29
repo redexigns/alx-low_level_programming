539a1e15b834437d24e9d969cafb9f628d55cafe
Jerry Jehu <jerryjehuprecious@gmail.com>
2022-09-29 06:40:55 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _ strlen_recursion - Return the legnth of the string
+* @s: string to be printed
+*
+* Return: void
+*/
+int _strlen_recursion(char *s)
+{
+	if int sum = 0;
+
+	if (*s != '\0')
+	{
+		sum++;
+		sum += _strlen_recursion(s + 1);
+	}
+	return (sum);
+}
