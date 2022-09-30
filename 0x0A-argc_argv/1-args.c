fea63e16503a9251f9895e11898fa439a53ce489
Jerry Jehu <jerryjehuprecious@gmail.com>
2022-09-30 10:42:27 -0700

    Taask 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio>
+#include <stdlib>
+
+/**
+* main - program that prints the number of arguements passed into it
+*@argc: argument count
+*@argv: argument vector
+*Return: Always Zero
+*/
+
+int main(int argc, char **argv)
+{
+	int num;
+	for (num = 0; num < argc;)
+	{
+		num++;
+		(*argv)++;
+	}
+	printf("%i\n", num - 1);
+	return (0);
+}
