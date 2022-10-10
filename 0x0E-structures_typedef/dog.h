20e9b86b8961be6dc7b51020735b686b3d908a5c
Jerry Jehu <jerryjehuojonugwa@gmail.com>
2022-10-10 16:59:27 -0700

    task dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,11 @@
 #define DOG_H_FILE
 
 /**
- *  * struct dog - structure definition of a dog
- *   * @name: dog name
- *    * @age: dog age
- *     * @owner: owner
- *      */
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
 typedef struct dog
 {
 		char *name;
